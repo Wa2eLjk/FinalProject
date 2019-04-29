a546c195a86a881a8d18262424a8c6999f9e01a8
Steve Jobs <w.alsheheli@gmail.com>
2019-04-29 22:05:39 +0300

    Login Page

---
+++ Wa2eL/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    $('#submit').on('click',function(){
+        var userEmail = $('#email').val();
+        var userPass = $('#password').val();
+        
+        console.log(userEmail);
+        console.log(userPass);
+    })
+        
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
